2c7dd7627acc6cf9a6bd162aff50734885ea7a69
aziza-iliasova <miacorporation25@gmail.com>
2021-08-04 01:11:22 +0300

    Add current version of navbar

--- src/App.jsx
+++ src/App.jsx
@@ -7,28 +7,31 @@ import { Container } from "react-bootstrap";
 // router
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+// components
+import NavBar from "./components/NavBar";
+
 function App() {
   return (
-    <Container style={{ minHeight: "100vh" }}>
+    <Container fluid style={{ minHeight: "100vh", padding: "0" }}>
       <Router>
-        <p>Navbar</p>
+        <NavBar />
         <Switch>
           <Route exact path="/home">
             <p>Home</p>
           </Route>
-          <Route exact path="/profile">
+          <Route path="/profile">
             <p>Profile</p>
           </Route>
-          <Route exact path="/users">
+          <Route path="/users">
             <p>Users</p>
           </Route>
-          <Route exact path="/meet">
+          <Route path="/meet">
             <p>Meet</p>
           </Route>
-          <Route exact path="/sign-in">
+          <Route path="/signin">
             <p>Sign In</p>
           </Route>
-          <Route exact path="/sign-up">
+          <Route path="/signup">
             <p>Sign Up</p>
           </Route>
         </Switch>
