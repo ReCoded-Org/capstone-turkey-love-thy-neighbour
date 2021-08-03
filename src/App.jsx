97c1052bd13f84e50ca471a1f0fbbe8d7c32b996
aziza-iliasova <miacorporation25@gmail.com>
2021-08-03 22:07:39 +0300

    setup the routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,39 @@
+// react
+import React from "react";
+
+// react-bootstrap
+import { Container } from "react-bootstrap";
+
+// router
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-      <p>This project has officially begun folks!</p>
-    </div>
+    <Container style={{ minHeight: "100vh" }}>
+      <Router>
+        <p>Navbar</p>
+        <Switch>
+          <Route exact path="/home">
+            <p>Home</p>
+          </Route>
+          <Route exact path="/profile">
+            <p>Profile</p>
+          </Route>
+          <Route exact path="/users">
+            <p>Users</p>
+          </Route>
+          <Route exact path="/meet">
+            <p>Meet</p>
+          </Route>
+          <Route exact path="/sign-in">
+            <p>Sign In</p>
+          </Route>
+          <Route exact path="/sign-up">
+            <p>Sign Up</p>
+          </Route>
+        </Switch>
+      </Router>
+    </Container>
   );
 }
 
