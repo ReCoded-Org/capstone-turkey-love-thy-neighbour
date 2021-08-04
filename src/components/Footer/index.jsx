fbaeacfeac073b8241f47ae6319af18878a9a517
aziza-iliasova <miacorporation25@gmail.com>
2021-08-04 20:58:03 +0300

    Add footer folder in components folder

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+// react
+import React from "react";
+
+// react-bootstrap
+import { Container } from "react-bootstrap";
+
+function Footer() {
+  return <Container></Container>;
+}
+
+export default Footer;
