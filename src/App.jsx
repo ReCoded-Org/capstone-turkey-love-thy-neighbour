09445d5d42847820fc833e7763b853258a1e1f73
Ali Rıza <ars.lifestyle@hotmail.com>
2021-08-04 11:32:34 +0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="App">
-      <p>This project has officially begun folks!</p>
+      <p>This project has officially begun folks!!</p>
     </div>
   );
 }
