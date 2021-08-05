ce9c486846727cedba352bef9dcd2eecc6957ffc
Ali Rıza <ars.lifestyle@hotmail.com>
2021-08-05 21:08:56 +0300

    Add right column img and align everything

--- src/containers/Home/PrimaryHero/index.jsx
+++ src/containers/Home/PrimaryHero/index.jsx
@@ -5,29 +5,34 @@ import { Container, Row, Col } from "react-bootstrap";
 import styled from "styled-components";
 
 import "./index.scss";
+import img from "../../../utils/PrimaryHeroContentBgImg.png";
+// sm and xs screen sizes wil be displayed in a one column layout
 
 function PrimaryHero() {
   const CATButton = styled.button`
     background-color: transparent;
     border: none;
+    border-radius: 5px;
     color: white;
     padding: 13px 65px;
     margin-top: 13px;
   `;
 
   return (
-    <section className="hero1-img-bg container-fluid">
-      <Container className="hero1-content-container">
-        <Row className="hero1-content-container">
+    <section className="primary-hero-img-bg container-fluid">
+      <Container className="primary-hero-content-container d-flex align-items-center">
+        <Row className="primary-hero-content-row">
           <Col className="pe-0 d-flex flex-column justify-content-center align-items-start">
-            <h1 className="fs-1">Love Thy Neighbor!</h1>
+            <h1>Love Thy Neighbor!</h1>
             <p>
               Let’s build a strong, dynamic and caring community that uplifts
               each other.
             </p>
             <CATButton className="cta-button">Join Us!</CATButton>
           </Col>
-          <Col className="ps-0 right-column">a</Col>
+          <Col className="p-0">
+            <img src={img} alt="img" className="img-fluid" />
+          </Col>
         </Row>
       </Container>
     </section>
