93dcc3504d97bb4ff6bbe085739d35063987b4b7
Ali Rıza <ars.lifestyle@hotmail.com>
2021-08-05 23:33:01 +0300

    Add responsiveness and images

--- src/containers/Home/PrimaryHero/index.jsx
+++ src/containers/Home/PrimaryHero/index.jsx
@@ -21,8 +21,13 @@ function PrimaryHero() {
   return (
     <section className="primary-hero-img-bg container-fluid">
       <Container className="primary-hero-content-container d-flex align-items-center">
-        <Row className="primary-hero-content-row">
-          <Col className="pe-0 d-flex flex-column justify-content-center align-items-start">
+        <Row className="primary-hero-content-row m-0">
+          <Col
+            xs={12}
+            sm={12}
+            md={6}
+            className="p-0 pb-xs-5 pb-md-5 d-flex flex-column justify-content-center align-items-start"
+          >
             <h1>Love Thy Neighbor!</h1>
             <p>
               Let’s build a strong, dynamic and caring community that uplifts
@@ -30,7 +35,7 @@ function PrimaryHero() {
             </p>
             <CATButton className="cta-button">Join Us!</CATButton>
           </Col>
-          <Col className="p-0">
+          <Col xs={12} sm={12} md={6} className="p-0 px-xs-1 px-md-1 h-100">
             <img src={img} alt="img" className="img-fluid" />
           </Col>
         </Row>
