ceaccf3e6f5d6fbb4c78a959f77f83186caec0d0
Ali Rıza <ars.lifestyle@hotmail.com>
2021-08-07 22:42:49 +0300

    Create and add custimozable button into primary hero section

--- src/components/CustomButton.jsx
+++ src/components/CustomButton.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import styled from "styled-components";
+import PropTypes from "prop-types";
 
-function CustomButton() {
-  return <div />;
+const Button = styled.button`
+  border: none;
+  border-radius: 5px;
+  background-color: var(--dark-blue);
+`;
+
+function CustomButton({ children, className }) {
+  return <Button className={className}>{children}</Button>;
 }
+
+CustomButton.propTypes = {
+  children: PropTypes.node.isRequired,
+  className: PropTypes.string,
+};
+
+CustomButton.defaultProps = {
+  className: true,
+};
 
 export default CustomButton;
