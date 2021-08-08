982355fbb80f68c8bcabbdfdfd330cdd3621bc23
MoulhamHallak <mou990@gmail.com>
2021-08-08 15:58:33 +0300

    delete unecessary public folder add more formik

--- src/components/EditProfileModal/index.jsx
+++ src/components/EditProfileModal/index.jsx
@@ -4,6 +4,42 @@ import { useFormik } from "formik";
 import "./index.scss";
 
 const EditProfileModal = ({ handleClick, showModal }) => {
+  // TODO: We need to add the validation errors for other stuff.
+  const validate = (values) => {
+    const errors = {};
+
+    if (!values.firstName) {
+      errors.firstName = "Required";
+    }
+    if (!values.lastName) {
+      errors.lastName = "Required";
+    }
+    if (!values.email) {
+      errors.email = "Required";
+    }
+    if (!values.gender) {
+      errors.gender = "Required";
+    }
+    if (!values.age) {
+      errors.age = "Required";
+    }
+    if (!values.education) {
+      errors.education = "Required";
+    }
+    if (!values.bio) {
+      errors.bio = "Required";
+    }
+    if (!values.number) {
+      errors.number = "Required";
+    }
+    if (!values.address) {
+      errors.address = "Required";
+    }
+    if (!values.interests) {
+      errors.interests = "Required";
+    }
+    return errors;
+  };
   const formik = useFormik({
     initialValues: {
       firstName: "",
@@ -19,6 +55,7 @@ const EditProfileModal = ({ handleClick, showModal }) => {
       profileImageUrl: "",
       backgroundImageUrl: "",
     },
+    validate,
     onSubmit: (values) => {
       alert(JSON.stringify(values, null, 2));
     },
@@ -42,7 +79,11 @@ const EditProfileModal = ({ handleClick, showModal }) => {
                   placeholder="First Name"
                   onChange={formik.handleChange}
                   value={formik.values.firstName}
+                  onBlur={formik.handleBlur}
                 />
+                {formik.touched.firstName && formik.errors.firstName ? (
+                  <div className="error-msg">{formik.errors.firstName}</div>
+                ) : null}
                 <input
                   id="lastName"
                   name="lastName"
@@ -50,8 +91,11 @@ const EditProfileModal = ({ handleClick, showModal }) => {
                   placeholder="Last Name"
                   onChange={formik.handleChange}
                   value={formik.values.lastName}
+                  onBlur={formik.handleBlur}
                 />
-
+                {formik.touched.lastName && formik.errors.lastName ? (
+                  <div className="error-msg">{formik.errors.lastName}</div>
+                ) : null}
                 <section>
                   <input
                     id="email"
@@ -60,26 +104,43 @@ const EditProfileModal = ({ handleClick, showModal }) => {
                     placeholder="District"
                     onChange={formik.handleChange}
                     value={formik.values.email}
+                    onBlur={formik.handleBlur}
                   />
+                  {formik.touched.email && formik.errors.email ? (
+                    <div className="error-msg">{formik.errors.email}</div>
+                  ) : null}
                 </section>
 
                 <section>
-                  <input
-                    id="gender"
+                  <select
                     name="gender"
-                    type="gender"
-                    placeholder="Gender"
                     onChange={formik.handleChange}
                     value={formik.values.gender}
-                  />
+                    onBlur={formik.handleBlur}
+                  >
+                    <option value="male">Male </option>
+                    <option value="female">Female </option>
+                    <option value="other">Prefer not to say</option>
+                  </select>
+                  {formik.touched.gender && formik.errors.gender ? (
+                    <div className="error-msg">{formik.errors.gender}</div>
+                  ) : null}
+
                   <input
                     id="age"
                     name="age"
-                    type="age"
+                    type="number"
                     placeholder="Age"
+                    min="15"
+                    max="99"
                     onChange={formik.handleChange}
                     value={formik.values.age}
+                    onBlur={formik.handleBlur}
                   />
+
+                  {formik.touched.age && formik.errors.age ? (
+                    <div className="error-msg">{formik.errors.age}</div>
+                  ) : null}
                 </section>
                 <section>
                   <input
@@ -89,7 +150,11 @@ const EditProfileModal = ({ handleClick, showModal }) => {
                     placeholder="Education"
                     onChange={formik.handleChange}
                     value={formik.values.education}
+                    onBlur={formik.handleBlur}
                   />
+                  {formik.touched.education && formik.errors.education ? (
+                    <div className="error-msg">{formik.errors.education}</div>
+                  ) : null}
                 </section>
                 <section>
                   <input
@@ -99,7 +164,11 @@ const EditProfileModal = ({ handleClick, showModal }) => {
                     placeholder="Bio"
                     onChange={formik.handleChange}
                     value={formik.values.bio}
+                    onBlur={formik.handleBlur}
                   />
+                  {formik.touched.bio && formik.errors.bio ? (
+                    <div className="error-msg">{formik.errors.bio}</div>
+                  ) : null}
                 </section>
                 <section>
                   <input
@@ -109,47 +178,73 @@ const EditProfileModal = ({ handleClick, showModal }) => {
                     placeholder="Interests"
                     onChange={formik.handleChange}
                     value={formik.values.interests}
+                    onBlur={formik.handleBlur}
                   />
+                  {formik.touched.interests && formik.errors.interests ? (
+                    <div className="error-msg">{formik.errors.interests}</div>
+                  ) : null}
                 </section>
                 <section>
                   <input
                     id="number"
                     name="number"
-                    type="number"
+                    type="tel"
                     placeholder="Number"
                     onChange={formik.handleChange}
                     value={formik.values.number}
+                    onBlur={formik.handleBlur}
                   />
+                  {formik.touched.number && formik.errors.number ? (
+                    <div className="error-msg">{formik.errors.number}</div>
+                  ) : null}
                 </section>
                 <section>
                   <input
                     id="address"
                     name="address"
                     type="address"
-                    placeholder="Adress"
+                    placeholder="Address"
                     onChange={formik.handleChange}
                     value={formik.values.address}
+                    onBlur={formik.handleBlur}
                   />
+                  {formik.touched.address && formik.errors.address ? (
+                    <div className="error-msg">{formik.errors.address}</div>
+                  ) : null}
                 </section>
                 <section>
                   <input
                     id="profileImageUrl"
                     name="profileImageUrl"
-                    type="profileImageUrl"
+                    type="url"
                     placeholder="Profile Image URL"
                     onChange={formik.handleChange}
                     value={formik.values.profileImageUrl}
+                    onBlur={formik.handleBlur}
                   />
+                  {formik.touched.profileImageUrl &&
+                  formik.errors.profileImageUrl ? (
+                    <div className="error-msg">
+                      {formik.errors.profileImageUrl}
+                    </div>
+                  ) : null}
                 </section>
                 <section>
                   <input
                     id="backgroundImageUrl"
                     name="backgroundImageUrl"
-                    type="backgroundImageUrl"
+                    type="url"
                     placeholder="Background Image URL"
                     onChange={formik.handleChange}
                     value={formik.values.backgroundImageUrl}
+                    onBlur={formik.handleBlur}
                   />
+                  {formik.touched.backgroundImageUrl &&
+                  formik.errors.backgroundImageUrl ? (
+                    <div className="error-msg">
+                      {formik.errors.backgroundImageUrl}
+                    </div>
+                  ) : null}
                 </section>
               </Card.Text>
             </Card.Body>
@@ -158,7 +253,7 @@ const EditProfileModal = ({ handleClick, showModal }) => {
 
         <Modal.Footer>
           <Button>Discard Changes</Button>
-          <Button>Save Changes</Button>
+          <Button type="submit">Save Changes</Button>
         </Modal.Footer>
       </form>
     </Modal>
