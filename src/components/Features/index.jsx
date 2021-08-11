4dcb2ed9ee890bb9c1e074ac4928fb87646dc751
Ali Rıza <ars.lifestyle@hotmail.com>
2021-08-11 20:04:53 +0300

    Add images for cards

--- src/components/Features/index.jsx
+++ src/components/Features/index.jsx
@@ -2,14 +2,21 @@ import React from "react";
 
 import { Container, Row, Col, Card } from "react-bootstrap";
 
+import NetworkImg from "../../images/network.svg";
+import SpeechBubbleImg from "../../images/speech-bubble.svg";
+import CouponImg from "../../images/coupon.svg";
+
+import "./index.scss";
+
 function Features() {
   return (
     <Container fluid>
       <Container>
         <h2>Features</h2>
-        <Row>
+        <Row className="features-content-row">
           <Col>
-            <Card style={{ width: "18rem" }}>
+            <Card className="d-flex flex-column align-items-center">
+              <img src={NetworkImg} className="img-fluid" alt="network svg" />
               <Card.Body>
                 <Card.Title>Card Title</Card.Title>
                 <Card.Text>
@@ -20,7 +27,12 @@ function Features() {
             </Card>
           </Col>
           <Col>
-            <Card style={{ width: "18rem" }}>
+            <Card className="d-flex flex-column align-items-center">
+              <img
+                src={SpeechBubbleImg}
+                className="img-fluid"
+                alt="speech bubble svg"
+              />
               <Card.Body>
                 <Card.Title>Card Title</Card.Title>
                 <Card.Text>
@@ -31,14 +43,13 @@ function Features() {
             </Card>
           </Col>
           <Col>
-            <Card style={{ width: "18rem" }}>
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card&lsquo;s content.
-                </Card.Text>
-              </Card.Body>
+            <Card className="d-flex flex-column align-items-center justify-content-center">
+              <img src={CouponImg} className="img-fluid" alt="coupon svg" />
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card&lsquo;s content.
+              </Card.Text>
             </Card>
           </Col>
         </Row>
