a083fd525ba787b1df65e02544eb281156aade84
aziza-iliasova <miacorporation25@gmail.com>
2021-08-12 22:17:17 +0300

    Add subscription section

---
+++ src/components/Subscription/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./index.scss";
+
+function Subscription() {
+  return <h1>Last Section</h1>;
+}
+
+export default Subscription;
--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -2,18 +2,20 @@ import React from "react";
 
 import { Container } from "react-bootstrap";
 
-import FAQ from "../../components/FAQ";
-import PrimaryHero from "../../components/PrimaryHero";
-import SecondaryHero from "../../components/SecondaryHero";
+// import FAQ from "../../components/FAQ";
+import Subscription from "../../components/Subscription";
+// import PrimaryHero from "../../components/PrimaryHero";
+// import SecondaryHero from "../../components/SecondaryHero";
 
 import "./index.scss";
 
 function Home() {
   return (
     <Container fluid className="section-wrapper">
-      <PrimaryHero />
+      {/* <PrimaryHero />
       <SecondaryHero />
-      <FAQ />
+      <FAQ /> */}
+      <Subscription />
     </Container>
   );
 }
