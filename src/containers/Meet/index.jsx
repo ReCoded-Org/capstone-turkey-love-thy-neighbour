df4c63475c8207c0edb0adcf0fb83f21b1dcdbea
MoulhamHallak <mou990@gmail.com>
2021-08-13 20:46:05 +0300

    Add initialiser code for the meet page

--- src/containers/Meet/index.jsx
+++ src/containers/Meet/index.jsx
@@ -1,11 +1,29 @@
+/* eslint-disable prettier/prettier */ 
 import React from "react";
 
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
+
+import { CTAButton } from "../../components/CustomButtons/index"
+import MeetImg from "../../images/meet.png"
+import "./index.scss"
+// use https://lipis.github.io/bootstrap-sweetalert/ the second from the bottom for the how was your meeting alert
 
 function Meet() {
   return (
-    <Container>
-      <p>Meet</p>
+    <Container fluid className="meet-container-fluid">
+      <Container className="meet-container">
+      <Row className="meet-row">
+        <Col>
+        <h3>Hello, Ali</h3>
+        <h1>Meet nearby people with our cutting edge algorithms.</h1>
+        <p>After you have been matched with people near you, you can send them a message and meet up quickly !</p>
+        <CTAButton>Meet Now</CTAButton>
+        </Col>
+        <Col>
+        <img src={MeetImg} className="img-fluid" alt="meet img" />
+        </Col>
+      </Row>
+      </Container>
     </Container>
   );
 }
