f99c337d1d283b922692e0a20c7efaa33be21b3a
iseluin <kutay.kagan.ozenn@gmail.com>
2021-08-14 20:51:58 +0300

    Add popup reducer to the root reducer

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import userReducer from "./userSlice";
+import popupReducer from "./popupSlice";
 
 const rootReducer = combineReducers({
   user: userReducer,
+  popup: popupReducer,
 });
 
 export default rootReducer;
