3ff13cc002ea1075b7794f256ad9b0e06a5d8bf0
aziza-iliasova <miacorporation25@gmail.com>
2021-08-16 22:10:20 +0300

    Deleted unnecessary files

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -2,19 +2,13 @@ import React from "react";
 
 import { Container } from "react-bootstrap";
 
-// import FAQ from "../../components/FAQ";
 import Subscription from "../../components/Subscription";
-// import PrimaryHero from "../../components/PrimaryHero";
-// import SecondaryHero from "../../components/SecondaryHero";
 
 import "./index.scss";
 
 function Home() {
   return (
     <Container fluid className="section-wrapper">
-      {/* <PrimaryHero />
-      <SecondaryHero />
-      <FAQ /> */}
       <Subscription />
     </Container>
   );
