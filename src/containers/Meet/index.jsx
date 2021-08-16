63d21ae0881e4172c29b4493f27f4e7290637279
Ali Rıza <ars.lifestyle@hotmail.com>
2021-08-17 00:37:24 +0300

    Redirect users to neigbors page after clicking meet now button

--- src/containers/Meet/index.jsx
+++ src/containers/Meet/index.jsx
@@ -5,10 +5,14 @@ import { Container, Row, Col } from "react-bootstrap";
 
 import { useSelector } from "react-redux";
 
+import { Link } from "react-router-dom";
+
 import { CTAButton } from "../../components/CustomButtons/index";
 import MeetImg from "../../images/meet.jpg";
 import "./index.scss";
-// use https://lipis.github.io/bootstrap-sweetalert/ the second from the bottom for the how was your meeting alert
+
+// TODO: redirect users after sign in into here
+// TODO: use https://lipis.github.io/bootstrap-sweetalert/ the second from the bottom for the how was your meeting alert
 
 function Meet() {
   const firstName = useSelector((state) => state.user.firestoreDoc?.firstName);
@@ -17,14 +21,21 @@ function Meet() {
     <Container fluid className="meet-container-fluid d-flex align-items-center">
       <Container className="meet-content-container d-flex align-items-center h-100">
         <Row className="meet-row">
-          <Col xs={12} sm={12} md={6} className="d-flex flex-column justify-content-center align-items-start mt-5 mt-sm-5 mt-md-0">
+          <Col
+            xs={12}
+            sm={12}
+            md={6}
+            className="d-flex flex-column justify-content-center align-items-start mt-5 mt-sm-5 mt-md-0"
+          >
             <h3>Hello, {firstName}</h3>
             <h1>Meet nearby people with our cutting edge algorithms.</h1>
             <p>
               After you have been matched with people near you, you can send
               them a message and meet up quickly !
             </p>
-            <CTAButton>Meet Now</CTAButton>
+            <CTAButton>
+              <Link to="/neighbors">Meet Now</Link>
+            </CTAButton>
           </Col>
           <Col xs={12} sm={12} md={6} className="d-flex justify-content-end">
             <img src={MeetImg} className="img-fluid" alt="meet img" />
