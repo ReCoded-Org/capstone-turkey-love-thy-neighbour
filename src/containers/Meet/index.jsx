a24459f2424d19102950b04103eac5ad975a6751
MoulhamHallak <mou990@gmail.com>
2021-08-16 23:59:23 +0300

    Add media queries

--- src/containers/Meet/index.jsx
+++ src/containers/Meet/index.jsx
@@ -3,18 +3,22 @@ import React from "react";
 
 import { Container, Row, Col } from "react-bootstrap";
 
+import { useSelector } from "react-redux";
+
 import { CTAButton } from "../../components/CustomButtons/index";
 import MeetImg from "../../images/meet.jpg";
 import "./index.scss";
 // use https://lipis.github.io/bootstrap-sweetalert/ the second from the bottom for the how was your meeting alert
 
 function Meet() {
+  const firstName = useSelector((state) => state.user.firestoreDoc?.firstName);
+
   return (
     <Container fluid className="meet-container-fluid">
-      <Container className="meet-container d-flex align-items-center">
+      <Container className="meet-content-container d-flex align-items-center">
         <Row className="meet-row">
-          <Col xs={12} sm={12} md={6}>
-            <h3>Hello, Ali</h3>
+          <Col xs={12} sm={12} md={6} className="d-flex flex-column justify-content-center align-items-start">
+            <h3>Hello, {firstName}</h3>
             <h1>Meet nearby people with our cutting edge algorithms.</h1>
             <p>
               After you have been matched with people near you, you can send
