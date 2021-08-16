12374edd647a8d22d7bfd0f5d6cba558a4fb745a
Ali Rıza <ars.lifestyle@hotmail.com>
2021-08-17 01:06:02 +0300

    Add starter layout for the neighbors page

--- src/containers/Neighbors/index.jsx
+++ src/containers/Neighbors/index.jsx
@@ -1,11 +1,32 @@
 import React from "react";
 
-import { Container } from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
+
+import "./index.scss";
 
 function Neighbors() {
   return (
-    <Container>
-      <p>Neighbors</p>
+    <Container fluid className="neighbors-container-fluid">
+      <Container className="neighbors-content-container">
+        <div className="my-3">
+          <h1>Nearby neighbors to meet :</h1>
+          <div className="filter-wrapper">
+            <select>
+              <option>option 1</option>
+              <option>option 2</option>
+              <option>option 3</option>
+              <option>option 4</option>
+            </select>
+            <select>
+              <option>option 1</option>
+              <option>option 2</option>
+              <option>option 3</option>
+              <option>option 4</option>
+            </select>
+          </div>
+        </div>
+        <Row>hey</Row>
+      </Container>
     </Container>
   );
 }
