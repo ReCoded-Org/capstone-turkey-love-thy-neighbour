f729bf16b7090691a7d56e33ae841045f21cccf9
Ali Rıza <ars.lifestyle@hotmail.com>
2021-08-17 20:06:44 +0300

    update import in profile

--- src/containers/Profile/index.jsx
+++ src/containers/Profile/index.jsx
@@ -4,7 +4,7 @@ import { Container, Row, Col, Card } from "react-bootstrap";
 
 import { useDispatch, useSelector } from "react-redux";
 
-import PPMaleSVG from "../../images/ProfilePage/PPMaleSVG.svg";
+import PPMaleSVG from "../../images/Profile/PPMaleSVG.svg";
 import EditProfileModal from "../../components/EditProfileModal";
 import { EditProfileButton } from "../../components/CustomButtons/index";
 import "./index.scss";
