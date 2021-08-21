87a5f3570c62256a113914bb08a639069ecb5601
aziza-iliasova <miacorporation25@gmail.com>
2021-08-21 03:55:39 +0300

    Add svg icon file

--- src/components/Subscription/index.jsx
+++ src/components/Subscription/index.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 
 import PhoneAndNotificationsImg from "../../images/PhoneandNotifications.svg";
+import { ReactComponent as CheckIcon } from "../../images/Check.svg";
 
 import "./index.scss";
 
@@ -42,16 +43,7 @@ function Subscription() {
                 className="btn btn-primary"
                 disabled={!value}
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  className="bi bi-check-lg"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M13.485 1.431a1.473 1.473 0 0 1 2.104 2.062l-7.84 9.801a1.473 1.473 0 0 1-2.12.04L.431 8.138a1.473 1.473 0 0 1 2.084-2.083l4.111 4.112 6.82-8.69a.486.486 0 0 1 .04-.045z" />
-                </svg>
+                <CheckIcon />
               </button>
             </div>
           </Col>
