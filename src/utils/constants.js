a5bfafc5a08017012631c27d5224e8ecb672bfdb
Ali Rıza <ars.lifestyle@hotmail.com>
2021-08-24 00:15:47 +0300

    Add the recommended places data for two districts

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -68,4 +68,82 @@ export const newActivityList = constants.activityList.map((exercise) => {
   };
 });
 
+export const recommendedPlaces = {
+  KADIKÖY: [
+    {
+      name: "Ağaç Ev",
+      image:
+        "https://b.zmtcdn.com/data/pictures/3/18970253/105a380e6ad9ccdf63b8c9939273ce6f.jpg",
+      address:
+        "Osmanağa, No: 34104, Osmancık Sk. No:13 B, 34714 Kadıköy/İstanbul",
+    },
+    {
+      name: "Dorock XL",
+      address:
+        "Caferağa, Namlı Market Yanı, Neşet Ömer Sk. 3C, 34710 Kadıköy/İstanbul ",
+      image:
+        "https://b.zmtcdn.com/data/pictures/1/18016871/7548fd97977dd16fc4448828a36dac35.jpg",
+    },
+    {
+      name: "Starbucks",
+      address: "Caferağa Mah, Muvakkıthane Cd. No:9, 34710 Kadıköy/İstanbul",
+      image:
+        "https://emoji.com.tr/wp-content/uploads/2017/12/en-guzel-starbucks-subeleri-019.jpg",
+    },
+  ],
+  BEYOĞLU: [
+    {
+      name: "https://sorabula.com.tr/wp-content/uploads/2020/06/james-joyce-irish-pub-1200x675.jpg",
+      image: "James Joyce Irish Pub",
+      address: "Balo Sk. No:26, 34435 Beyoğlu/İstanbul",
+    },
+    {
+      name: "Starbucks",
+      address:
+        "Merkez Mah. Abide-i Hürriyet Cad. No:211 Bolkan Center A-B-C Blok, 34384 Şişli/İstanbul",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScd0503peeHSqnRBynuMoUE5lK06lnsfh6gA&usqp=CAU",
+    },
+    {
+      name: "Dorock Heavy Metal Club",
+      address: "Hüseyinağa, Kameriye Sk. No 11/A, 34435 Beyoğlu/İstanbul",
+      image: "https://i.ibb.co/ZXQLFg3/drck-txm-1-jpg-1564052455-2.jpg",
+    },
+  ],
+  ŞİŞLİ: [
+    {
+      image: "",
+      address: "",
+      name: "",
+    },
+    {
+      image: "",
+      address: "",
+      name: "",
+    },
+    {
+      image: "",
+      address: "",
+      name: "",
+    },
+  ],
+  FATİH: [
+    {
+      image: "",
+      address: "",
+      name: "",
+    },
+    {
+      image: "",
+      address: "",
+      name: "",
+    },
+    {
+      image: "",
+      address: "",
+      name: "",
+    },
+  ],
+};
+
 export default constants;
