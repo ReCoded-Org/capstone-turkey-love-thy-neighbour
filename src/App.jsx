4ee9c2aae9138bc12d70be4e035495ac3b5c1b90
iseluin <kutay.kagan.ozenn@gmail.com>
2021-08-29 21:52:46 +0300

    Remove provider from app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 
-import Provider, { useDispatch, useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 import { Container } from "react-bootstrap";
 
