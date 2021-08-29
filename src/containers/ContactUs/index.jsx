22d52030c406de6110d81fe0b879e2a72c68ede8
aziza-iliasova <miacorporation25@gmail.com>
2021-08-30 00:59:53 +0300

    Add pop-up

--- src/containers/ContactUs/index.jsx
+++ src/containers/ContactUs/index.jsx
@@ -159,7 +159,7 @@ function ContactUs() {
                     <Modal.Header />
                     <Modal.Body>
                       <h5>
-                        The message was successfully sent!
+                        The message was successfully sent.
                         <br />
                         We will get you back soon!
                       </h5>
