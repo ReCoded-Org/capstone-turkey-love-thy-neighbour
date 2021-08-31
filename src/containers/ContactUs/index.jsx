b2444b2f689f9e8fa715b4a27f540d6ab337989a
aziza-iliasova <miacorporation25@gmail.com>
2021-08-31 21:02:32 +0300

    Last changes

--- src/containers/ContactUs/index.jsx
+++ src/containers/ContactUs/index.jsx
@@ -157,14 +157,16 @@ function ContactUs() {
                   </CTAButton>
                   <Modal show={show} onHide={handleClose}>
                     <Modal.Header>
-                      <img
-                        style={{
-                          height: 150,
-                          width: 150,
-                        }}
-                        src="https://simg.nicepng.com/png/small/29-290368_banner-library-library-check-icon-svg-public-domain.png"
-                        alt="img"
-                      />
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="20%"
+                        height="20%"
+                        fill="green"
+                        className="check-icon"
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="M13.485 1.431a1.473 1.473 0 0 1 2.104 2.062l-7.84 9.801a1.473 1.473 0 0 1-2.12.04L.431 8.138a1.473 1.473 0 0 1 2.084-2.083l4.111 4.112 6.82-8.69a.486.486 0 0 1 .04-.045z" />
+                      </svg>
                     </Modal.Header>
                     <Modal.Body>
                       <h5>
