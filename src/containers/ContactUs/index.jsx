54784ba7b3009f49aeda2d4ed9b793c700400dce
aziza-iliasova <miacorporation25@gmail.com>
2021-09-01 21:47:05 +0300

    Validations work but firestore doesnt work

--- src/containers/ContactUs/index.jsx
+++ src/containers/ContactUs/index.jsx
@@ -1,17 +1,13 @@
 import React, { useState } from "react";
-
 import { Container, Row, Col, Button, Modal } from "react-bootstrap";
-
 import "./index.scss";
 import { useFormik } from "formik";
-
 import * as Yup from "yup";
 import { Link } from "react-router-dom";
 import { CTAButton } from "../../components/CustomButtons/index";
-
 import { ReactComponent as MainImg } from "../../images/firstContactUs.svg";
-import { ReactComponent as Logoimg } from "../../images/secondContactUs.svg";
-
+import { ReactComponent as LogoImg } from "../../images/secondContactUs.svg";
+import { ReactComponent as CheckIcon } from "../../images/checkicon.svg";
 import { firestore } from "../../firebaseConfig";
 
 function ContactUs() {
@@ -22,7 +18,7 @@ function ContactUs() {
       email: "",
       user_message: "",
     },
-    validationSchema: Yup.object({
+    validationSchema: Yup.object().shape({
       first_name: Yup.string()
         .min(2, "Mininum 2 characters")
         .max(15, "Maximum 15 characters")
@@ -37,37 +33,21 @@ function ContactUs() {
         .max(15, "Maximum 500 characters")
         .required("Required!"),
     }),
-    onSubmit: (values) => {
-      // eslint-disable-next-line
-      alert(JSON.stringify(values, null, 2));
+    onSubmit: (values, event) => {
+      firestore.collection("contacts").add({
+        first_name: values.first_name,
+        last_name: values.last_name,
+        email: values.email,
+        user_message: values.user_message,
+      });
     },
   });
 
-  const [name, setName] = useState("");
-  const [lastname, setLastname] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    firestore.collection("contacts").add({
-      name,
-      lastname,
-      email,
-      message,
-    });
-
-    setName("");
-    setLastname("");
-    setEmail("");
-    setMessage("");
-  };
   const [show, setShow] = useState(false);
-
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
   return (
-    <section className="contact-us-container">
+    <section className="container-fluid">
       <Container className="contactus-content-container d-flex align-items-center">
         <Row className="contactus-content-row m-0">
           <Col
@@ -82,8 +62,7 @@ function ContactUs() {
                 Help us create effective and active communities by communicating
                 with us!
               </h5>
-
-              <form onSubmit={(formik.handleSubmit, handleSubmit)}>
+              <form onSubmit={formik.handleSubmit}>
                 <div className="form-box">
                   <div className="first-last-names">
                     <div>
@@ -92,14 +71,14 @@ function ContactUs() {
                         type="text"
                         name="first_name"
                         placeholder="First Name"
-                        value={(formik.values.first_name, name)}
-                        onChange={
-                          (formik.handleChange, (e) => setName(e.target.value))
-                        }
+                        value={formik.values.first_name}
+                        onChange={formik.handleChange}
                       />
                       {formik.errors.first_name &&
                         formik.touched.first_name && (
-                          <p>{formik.errors.first_name}</p>
+                          <p className="validation">
+                            {formik.errors.first_name}
+                          </p>
                         )}
                     </div>
                     <div>
@@ -108,14 +87,11 @@ function ContactUs() {
                         type="text"
                         name="last_name"
                         placeholder="Last Name"
-                        value={(formik.values.last_name, lastname)}
-                        onChange={
-                          (formik.handleChange,
-                          (e) => setLastname(e.target.value))
-                        }
+                        value={formik.values.last_name}
+                        onChange={formik.handleChange}
                       />
                       {formik.errors.last_name && formik.touched.last_name && (
-                        <p>{formik.errors.last_name}</p>
+                        <p className="validation">{formik.errors.last_name}</p>
                       )}
                     </div>
                   </div>
@@ -125,13 +101,11 @@ function ContactUs() {
                       type="email"
                       name="email"
                       placeholder="Email"
-                      value={(formik.values.email, email)}
-                      onChange={
-                        (formik.handleChange, (e) => setEmail(e.target.value))
-                      }
+                      value={formik.values.email}
+                      onChange={formik.handleChange}
                     />
                     {formik.errors.email && formik.touched.email && (
-                      <p>{formik.errors.email}</p>
+                      <p className="validation">{formik.errors.email}</p>
                     )}
                   </div>
                   <div>
@@ -140,14 +114,14 @@ function ContactUs() {
                       type="text"
                       name="user_message"
                       placeholder="Message"
-                      value={(formik.values.user_message, message)}
-                      onChange={
-                        (formik.handleChange, (e) => setMessage(e.target.value))
-                      }
+                      value={formik.values.user_message}
+                      onChange={formik.handleChange}
                     />
                     {formik.errors.user_message &&
                       formik.touched.user_message && (
-                        <p>{formik.errors.user_message}</p>
+                        <p className="validation">
+                          {formik.errors.user_message}
+                        </p>
                       )}
                   </div>
                 </div>
@@ -156,21 +130,9 @@ function ContactUs() {
                     <Link to="/neighbors">Send</Link>
                   </CTAButton>
                   <Modal show={show} onHide={handleClose}>
-                    <Modal.Header>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="20%"
-                        height="20%"
-                        fill="green"
-                        className="check-icon"
-                        viewBox="0 0 16 16"
-                      >
-                        <path d="M13.485 1.431a1.473 1.473 0 0 1 2.104 2.062l-7.84 9.801a1.473 1.473 0 0 1-2.12.04L.431 8.138a1.473 1.473 0 0 1 2.084-2.083l4.111 4.112 6.82-8.69a.486.486 0 0 1 .04-.045z" />
-                      </svg>
-                    </Modal.Header>
                     <Modal.Body>
                       <h5>
-                        The message was successfully sent.
+                        The message was successfully sent! <CheckIcon />
                         <br />
                         We will get you back soon!
                       </h5>
@@ -187,12 +149,11 @@ function ContactUs() {
           </Col>
           <Col xs={12} sm={12} md={6} className="p-0 px-xs-1 px-md-1 h-100">
             <MainImg className="img-fluid main-img" />
-            <Logoimg className="img-fluid logo-img" />
+            <LogoImg alt="img" className="img-fluid logo-img" />
           </Col>
         </Row>
       </Container>
     </section>
   );
 }
-
 export default ContactUs;
