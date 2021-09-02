441c2c394a0c2010ce06cd6ee38e801002e5619f
Ali Rıza <ars.lifestyle@hotmail.com>
2021-09-02 18:51:54 +0300

    Remove colons from the headers

--- src/components/SocialProof/index.jsx
+++ src/components/SocialProof/index.jsx
@@ -16,14 +16,13 @@ function SocialProof() {
         <Row className="social-proof-content-row m-0 gap-2">
           <Col xs={12} sm={12} md={5} className="first-column">
             <h2 className="text-center text-sm-center text-md-start">
-              What our Neighbours say:
+              What our Neighbours say
             </h2>
             <Accordion
               defaultActiveKey={window.innerWidth >= 768 ? "0" : undefined}
             >
               <Accordion.Item className="first-accordion-item" eventKey="0">
                 <Accordion.Header className="d-flex align-items-center">
-                  {/* <img src={MaleAvatarSVG} alt="avatar svg" /> */}
                   <MaleAvatarSVG />
                   <div className="d-flex flex-column">
                     <p style={{ padding: "0", margin: "0" }}>Sammy Walson</p>
@@ -58,7 +57,6 @@ function SocialProof() {
               </Accordion.Item>
               <Accordion.Item className="third-accordion-item" eventKey="2">
                 <Accordion.Header className="d-flex align-items-center">
-                  {/* <img src={MaleAvatarSVG} alt="avatar svg" /> */}
                   <MaleAvatarSVG />
                   <div className="d-flex flex-column">
                     <p style={{ padding: "0", margin: "0" }}>
@@ -85,7 +83,7 @@ function SocialProof() {
             className="second-column mt-xs-4 mt-sm-4 mt-md-0"
           >
             <h2 className="text-center text-sm-center text-md-start">
-              When our Neighbours met:
+              When our Neighbours met
             </h2>
             <Carousel>
               <Carousel.Item>
