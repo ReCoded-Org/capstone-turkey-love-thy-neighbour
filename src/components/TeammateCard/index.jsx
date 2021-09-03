c9319d8b56fdfcb8304ed05a368c710a77ef2d3c
Ali Rıza <ars.lifestyle@hotmail.com>
2021-09-03 20:45:42 +0300

    Take the full name as first and last name to make a br between them

--- src/components/TeammateCard/index.jsx
+++ src/components/TeammateCard/index.jsx
@@ -6,37 +6,50 @@ import { ReactComponent as Twitter } from "../../images/twitter.svg";
 import { ReactComponent as Linkedn } from "../../images/linkedn.svg";
 import { ReactComponent as Github } from "../../images/github.svg";
 
-function TeammateCard({ image }) {
+import "./index.scss";
+
+function TeammateCard({
+  image,
+  githubUrl,
+  linkedInUrl,
+  twitterUrl,
+  jobTitle,
+  firstName,
+  lastName,
+}) {
   return (
-    <Col xs={12} sm={6} md={4} lg={3}>
-      <Card>
-        <img src={image} alt="teammate" className="img-fluid" />
-        <Card.Title> Shrreya Bhatachaarya </Card.Title>
-        <p>Lead Trainer</p>
-        <Col className="d-flex flex-row justify-content-center">
-          {" "}
-          <a
-            href="https://github.com/Shrreya"
-            target="_blank"
-            rel="external noreferrer"
-          >
-            <Github />
-          </a>{" "}
-          <a
-            href="https://www.linkedin.com/in/shrreya/"
-            target="_blank"
-            rel="external noreferrer"
-          >
-            <Linkedn />
-          </a>
-          <a
-            href="https://twitter.com/__shrreya__"
-            target="_blank"
-            rel="external noreferrer"
-          >
-            <Twitter />
-          </a>
-        </Col>
+    <Col
+      xs={12}
+      sm={6}
+      md={4}
+      className="teammate-card-column p-0 mb-3 mx-auto d-flex justify-content-center"
+    >
+      <Card className="teammate-card">
+        <Card.Img src={image} alt="teammate" />
+
+        <Card.Body className="p-0 d-flex flex-column justify-content-between">
+          <div className="mt-2">
+            <h2 className="mx-auto my-0 text-center">
+              {firstName}
+              <br />
+              {lastName}
+            </h2>
+            <Card.Text className="mx-auto text-center">{jobTitle}</Card.Text>
+          </div>
+
+          <div className="d-flex justify-content-around mb-2 mt-3">
+            {" "}
+            <a href={githubUrl} target="_blank" rel="external noreferrer">
+              <Github />
+            </a>{" "}
+            <a href={linkedInUrl} target="_blank" rel="external noreferrer">
+              <Linkedn />
+            </a>
+            <a href={twitterUrl} target="_blank" rel="external noreferrer">
+              <Twitter />
+            </a>
+          </div>
+        </Card.Body>
       </Card>
     </Col>
   );
