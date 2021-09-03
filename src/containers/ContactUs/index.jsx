957b897ec7937dc223e8bc5fc5481f1a9c67d095
Ali Rıza <ars.lifestyle@hotmail.com>
2021-09-03 15:17:18 +0300

    Remove modal code from here, create a component for it and use it by importing

--- src/containers/ContactUs/index.jsx
+++ src/containers/ContactUs/index.jsx
@@ -1,15 +1,22 @@
-import React, { useState } from "react";
-import { Container, Row, Col, Button, Modal } from "react-bootstrap";
-import "./index.scss";
-import { useFormik } from "formik";
+import React from "react";
+
+import { Container, Row, Col } from "react-bootstrap";
+
 import * as Yup from "yup";
-import { CTAButton } from "../../components/CustomButtons/index";
-import { ReactComponent as MainImg } from "../../images/firstContactUs.svg";
-import { ReactComponent as LogoImg } from "../../images/secondContactUs.svg";
-import { ReactComponent as CheckIcon } from "../../images/checkicon.svg";
+import { useFormik } from "formik";
+
+import { useDispatch, useSelector } from "react-redux";
 import { firestore } from "../../firebaseConfig";
 
+import EmailSentModal from "../../components/EmailSentModal";
+import { ContactUsSendButton } from "../../components/CustomButtons/index";
+import { ReactComponent as MainImg } from "../../images/firstContactUs.svg";
+import { ReactComponent as LogoSvg } from "../../images/secondContactUs.svg";
+import "./index.scss";
+
 function ContactUs() {
+  const dispatch = useDispatch();
+
   const formik = useFormik({
     initialValues: {
       first_name: "",
@@ -33,122 +40,94 @@ function ContactUs() {
         .required("Required!"),
     }),
     onSubmit: (values) => {
-      firestore.collection("contacts").add({
-        first_name: values.first_name,
-        last_name: values.last_name,
-        email: values.email,
-        user_message: values.user_message,
-      });
+      firestore
+        .collection("contacts")
+        .add({
+          first_name: values.first_name,
+          last_name: values.last_name,
+          email: values.email,
+          user_message: values.user_message,
+        })
+        .then(() => dispatch({ type: "emailSent" }));
     },
   });
 
-  const [show, setShow] = useState(false);
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
   return (
-    <section className="container-fluid">
-      <Container className="contactus-content-container d-flex align-items-center">
-        <Row className="contactus-content-row m-0">
+    <section className="contact-us-section container-fluid">
+      <Container className="contactus-content-container">
+        <Row className="contactus-content-row ">
           <Col
             xs={12}
             sm={12}
             md={6}
-            className="first-column p-0 pb-xs-5 pb-md-5 d-flex flex-column justify-content-center align-items-start"
+            className="d-flex flex-column justify-content-center align-items-center"
           >
-            <div className="App">
-              <h1 className="main-header">Contact Us</h1>
-              <h5>
-                Help us create effective and active communities by communicating
-                with us!
-              </h5>
-              <form onSubmit={formik.handleSubmit}>
-                <div className="form-box">
-                  <div className="first-last-names">
-                    <div>
-                      <input
-                        className="first-name"
-                        type="text"
-                        name="first_name"
-                        placeholder="First Name"
-                        value={formik.values.first_name}
-                        onChange={formik.handleChange}
-                      />
-                      {formik.errors.first_name &&
-                        formik.touched.first_name && (
-                          <p className="validation">
-                            {formik.errors.first_name}
-                          </p>
-                        )}
-                    </div>
-                    <div>
-                      <input
-                        className="last-name"
-                        type="text"
-                        name="last_name"
-                        placeholder="Last Name"
-                        value={formik.values.last_name}
-                        onChange={formik.handleChange}
-                      />
-                      {formik.errors.last_name && formik.touched.last_name && (
-                        <p className="validation">{formik.errors.last_name}</p>
-                      )}
-                    </div>
-                  </div>
-                  <div>
-                    <input
-                      className="email"
-                      type="email"
-                      name="email"
-                      placeholder="Email"
-                      value={formik.values.email}
-                      onChange={formik.handleChange}
-                    />
-                    {formik.errors.email && formik.touched.email && (
-                      <p className="validation">{formik.errors.email}</p>
-                    )}
-                  </div>
-                  <div>
-                    <textarea
-                      className="textarea"
-                      type="text"
-                      name="user_message"
-                      placeholder="Message"
-                      value={formik.values.user_message}
-                      onChange={formik.handleChange}
-                    />
-                    {formik.errors.user_message &&
-                      formik.touched.user_message && (
-                        <p className="validation">
-                          {formik.errors.user_message}
-                        </p>
-                      )}
-                  </div>
+            <h1>Contact Us</h1>
+            <p>
+              Help us create effective and active communities by communicating
+              with us!
+            </p>
+            <form className="d-flex flex-column" onSubmit={formik.handleSubmit}>
+              <div className="d-flex">
+                <div>
+                  <input
+                    type="text"
+                    name="first_name"
+                    placeholder="First Name"
+                    value={formik.values.first_name}
+                    onChange={formik.handleChange}
+                  />
+                  {formik.errors.first_name && formik.touched.first_name && (
+                    <div className="error-msg">{formik.errors.first_name}</div>
+                  )}
                 </div>
-                <div className="send-button">
-                  <CTAButton onClick={handleShow}>Send</CTAButton>
-                  <Modal show={show} onHide={handleClose}>
-                    <Modal.Body>
-                      <h5>
-                        The message was successfully sent! <CheckIcon />
-                        <br />
-                        We will get you back soon!
-                      </h5>
-                    </Modal.Body>
-                    <Modal.Footer>
-                      <Button variant="secondary" onClick={handleClose}>
-                        Close
-                      </Button>
-                    </Modal.Footer>
-                  </Modal>
+                <div>
+                  <input
+                    type="text"
+                    name="last_name"
+                    placeholder="Last Name"
+                    value={formik.values.last_name}
+                    onChange={formik.handleChange}
+                  />
+                  {formik.errors.last_name && formik.touched.last_name && (
+                    <div className="error-msg">{formik.errors.last_name}</div>
+                  )}
                 </div>
-              </form>
-            </div>
+              </div>
+              <input
+                type="email"
+                name="email"
+                placeholder="Email"
+                value={formik.values.email}
+                onChange={formik.handleChange}
+              />
+              {formik.errors.email && formik.touched.email && (
+                <div className="error-msg">{formik.errors.email}</div>
+              )}
+              <textarea
+                name="user_message"
+                placeholder="Message"
+                value={formik.values.user_message}
+                onChange={formik.handleChange}
+              />
+              {formik.errors.user_message && formik.touched.user_message && (
+                <div className="error-msg">{formik.errors.user_message}</div>
+              )}
+              <ContactUsSendButton>Send</ContactUsSendButton>
+            </form>
           </Col>
-          <Col xs={12} sm={12} md={6} className="p-0 px-xs-1 px-md-1 h-100">
+
+          <Col
+            xs={12}
+            sm={12}
+            md={6}
+            className="d-flex flex-column justify-content-center align-items-center"
+          >
             <MainImg className="img-fluid main-img" />
-            <LogoImg alt="img" className="img-fluid logo-img" />
+            <LogoSvg alt="img" className="img-fluid logo-img" />
           </Col>
         </Row>
+        <EmailSentModal />
       </Container>
     </section>
   );
